ab56877737468cb9a9fdd3097aad6b1cbfc7b10d
VladislavAduenko <adyenko.job@gmail.com>
2025-05-19 12:49:59 +0300

    teacher solution of coffee ordering

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 // import EmployeeForm from "./components/EmployeeForm";
-import CoffeeForm from "./components/CoffeeForm";
+// import CoffeeForm from "./components/CoffeeForm";
+import CoffeeOrderForm from "./components/CoffeeOrderForm";
 
 const App: React.FC = () => {
   return (
     //  <EmployeeForm submitter={(empl) => console.log(empl)}></EmployeeForm>
-    <CoffeeForm submitter={(coffee) => console.log(coffee)}></CoffeeForm>
+    // <CoffeeForm submitter={(coffee) => console.log(coffee)}></CoffeeForm>
+    <CoffeeOrderForm submitter={(coffeeOrder) => console.log(coffeeOrder)}></CoffeeOrderForm>
   );
 };
 
